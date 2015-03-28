37dd92ace895d859650748771cd7baf5fdb60905
Sunil <sunilvigneshr@gmail.com>
2015-03-28 21:20:11 +0530

    [Sunil] Ping endpoint in

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 var koa = require('koa');
 var app = koa();
+var route = require('koa-route');
+var serve = require('koa-static');
+var logger = require('koa-logger');
+
+app.use(serve('static'));
+app.use(logger());
+app.use(route.get('/ping', ping));
+
+function *ping(){
+  this.body = 'pong';
+}
 
 app.listen(3000);
+
